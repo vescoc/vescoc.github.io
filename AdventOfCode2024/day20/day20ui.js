9bacbb9adfd36a740c36293af9bae9fab50b1f4e
Vesco Claudio <claudio.vesco@gmail.com>
2024-12-20 23:50:41 +0100

    sync

--- AdventOfCode2024/day20/day20ui.js
+++ AdventOfCode2024/day20/day20ui.js
@@ -563,11 +563,11 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper299 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper298 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 72, __wbg_adapter_20);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper457 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper456 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 138, __wbg_adapter_23);
         return ret;
     };
